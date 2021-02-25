77d886ae270564ff864c8221effca13ffa5d6f4c
Maksim <maksim2103@yandex.ru>
2021-02-25 19:07:52 +0300

    added section Reviews

--- js/main.js
+++ js/main.js
@@ -5,4 +5,10 @@ $(function () {
     autoplay: true,
     fade: true,
   });
+  $('.reviews__slider').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  });
 });
